3688980a39a210d0466aff43d6f53ef8ffeb2e5a
mariatorresroman <116643212+mariatorresroman@users.noreply.github.com>
2023-03-21 13:39:25 +0100

    Create división.js

---
+++ división.js
@@ -0,0 +1,10 @@
+function división(a,b){
+    if(b== 0){
+        console.log("No se pude dividir entre 0")
+    }else{
+        return a/b;
+    }
+    }
+
+    let sol = división(a_1, a_2);
+    console.log("El resultado es:" + sol)
