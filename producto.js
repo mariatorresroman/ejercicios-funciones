8f7442c71187937325550dac81288bb386e8aee0
mariatorresroman <116643212+mariatorresroman@users.noreply.github.com>
2023-03-21 13:21:33 +0100

    Create producto.js

---
+++ producto.js
@@ -0,0 +1,12 @@
+//Ejercicio 1
+
+//Definir la función
+function producto(x1,x2){
+return x1 * x2;
+}
+
+//Llamada a la función
+let res_prod = producto (3,7);
+
+//Mostrar el resultado en la consola
+console.log("La multiplicación de 4 y 8 es:",resultado);
