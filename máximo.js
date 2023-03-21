f424d0a529c22a75a6c157e35fbf053964347a16
mariatorresroman <116643212+mariatorresroman@users.noreply.github.com>
2023-03-21 13:19:32 +0100

    Create máximo.js

---
+++ máximo.js
@@ -0,0 +1,11 @@
+//Ejercicio 4
+function máximo (a,b){
+    if(a>b){
+        return a;
+    }else{
+        return b;
+    }
+    }
+const rest_max = máximo(3,8);
+
+console.log("El número mayor de los dps es:", rest_max);
