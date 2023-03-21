4542bd05c339da1d1e03bbc748fced0bc0e5a4d0
mariatorresroman <116643212+mariatorresroman@users.noreply.github.com>
2023-03-21 13:36:10 +0100

    Create edad.js

---
+++ edad.js
@@ -0,0 +1,16 @@
+function edades(edad){
+    if(edad<13){
+        return "niño";
+    }else if(edad>=13 && edad<18){
+        return "adolescente";
+    }else if(edad>19 && edad<=64){
+        return "adulto";
+    }else{
+        return "mayor";
+    }
+    }
+    
+    console.log("10" + edades(20));
+    console.log("17" + edades(17));
+    console.log("24" + edades(24));
+    console.log("70" + edades(70));
