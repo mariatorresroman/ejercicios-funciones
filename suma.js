d2029891aa51db9fdfb10b22cc48a3bf7165daaf
mariatorresroman <116643212+mariatorresroman@users.noreply.github.com>
2023-03-21 13:20:13 +0100

    Create suma.js

---
+++ suma.js
@@ -0,0 +1,9 @@
+efinición de la función
+function suma(a,b){
+return a + b;
+}
+//Llamada a la función y almacenimiento del resultado de una variable
+const resultado = suma (4,8);
+
+//Mostar el resultado en la consola
+console.log("La suma de 4 y 8 es:",resultado);
